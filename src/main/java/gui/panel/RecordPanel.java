13432968815e690bce8435e1f5845f843a960e34
ryu <903996966@qq.com>
2020-09-13 00:49:05 +0800

    Revert "Revert "update""
    This reverts commit d884707dee87d07d9e3801909f92432449072c9a.

--- src/main/java/gui/panel/RecordPanel.java
+++ src/main/java/gui/panel/RecordPanel.java
@@ -9,7 +9,6 @@ public class RecordPanel extends WorkingPanel{
     /**
      * 这个面板采用BorderLayout 分为北面和中间。
      * 北面是一个JPanel，中间也是一个JPanel。
-     *
      * 北面的JPanel使用4行2列的 GridLayout。
      *
      * 中间的JPanel使用默认的FlowLayout,
